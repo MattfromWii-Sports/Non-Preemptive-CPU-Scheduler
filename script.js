2704516e10b887822f5379d11bf4039015d1a0f9
Matt <matthewjosefcruz@gmail.com>
2025-11-17 19:37:17 +0800

    Set Basic Webpage Design

---
+++ script.js
@@ -0,0 +1,14 @@
+// logic
+// 1. Solve for the gantt chart while updating the attributes of the page objects
+//         Stores the gantt values in an array
+// 2. Renders the table based on the object attributes
+// 3. Render the gantt chart
+//         Uses the gantt array
+// 4. Profit???
+
+// Render & calculate Table based on array with P objects
+// Only AT, BT, ST, CT (Prio for special cases) needed
+
+// Render Gantt chart based on array with "P" objects
+
+// Code under here runs immediately
